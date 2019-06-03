59254976b039b181c8486ad14d3ea97ce8ef9c5d
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-06-03 21:59:54 +0200

    Create the route to index

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'styles/atoms'
   get 'styles/molecules'
   get 'styles/organisms'
+  get 'ideas/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
