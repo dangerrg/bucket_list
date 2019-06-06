7cdabbeac0ac395c46eefed108bc0d200135ad5e
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-06-06 15:24:07 +0200

    Hook up the create idea form

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -7,8 +7,11 @@ class IdeasController < ApplicationController
 
   def new
   end
-  
+
   def create
+    idea = Idea.new
+    idea.title = params[:title]
+    idea.save!
     redirect_to(ideas_index_path)
   end
 end
