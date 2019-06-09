3834a1397915aa95b055303663d5e47c4d958d39
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-06-09 20:45:16 +0200

    Create edit and update actions for Ideas

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   get 'ideas/index'
   get '/ideas/new'
   post '/ideas/create'
+  get '/ideas/:id/edit', to: 'ideas#edit', as: 'edit_idea' 
+  patch '/ideas/:id', to: 'ideas#update', as: 'idea'
   get '/account/ideas'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
