a2d9ca646917f7df7ce918b67d7682c53b511064
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-06-11 21:21:11 +0200

    Modify test script parameters

--- test/system/using_capybara_test.rb
+++ test/system/using_capybara_test.rb
@@ -1,17 +1,17 @@
-require 'application_system_test_case' 
+require 'application_system_test_case'
 
-Capybara.run_server = false 
+Capybara.run_server = false
 
-class CapybaraTest < ApplicationSystemTestCase 
-  test 'capybara works' do 
-    visit('http://www.google.com') 
-    sleep(5.seconds) 
-    google_input = fill_in('q', with: 'Ruby on Rails') 
-    sleep(5.seconds) 
-    google_input.native.send_keys(:return) 
-    click_on('Ruby on Rails', match: :first) 
-    sleep(5.seconds) 
-    click_on('Guides') 
-    sleep(20.seconds) 
-  end 
-end 
+class CapybaraTest < ApplicationSystemTestCase
+  test 'capybara works' do
+    visit('http://www.google.com/en') #in this case the Firefox open in german so I added /en for english search
+    sleep(5.seconds)
+    google_input = fill_in('q', with: 'Cinque Terre')
+    sleep(5.seconds)
+    google_input.native.send_keys(:return)
+    click_on('Wikipedia', match: :first)
+    sleep(5.seconds)
+    click_on('Italian Riviera', match: :first)
+    sleep(20.seconds)
+  end
+end
