906bbf2947d13b2fc3f0bb14ab8cdceea705fab4
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-06-12 11:11:26 +0200

    Implement interaction with new Idea form

--- test/system/ideas_test.rb
+++ test/system/ideas_test.rb
@@ -1,9 +1,11 @@
 require "application_system_test_case"
 
 class IdeasTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit ideas_url
-  #
-  #   assert_selector "h1", text: "Ideas"
-  # end
+  test 'create new idea' do
+    visit(ideas_new_path)
+    fill_in('title', with: 'Skydiving in Interlaken')
+    fill_in('done_count', with: 65)
+    fill_in('photo_url', with: 'http://monkeychute.co/wp-content/uploads/2018/01/skydive-interlaken.jpg')
+    click_on('Create idea')
+  end
 end
