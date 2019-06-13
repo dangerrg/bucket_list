4b6ed68726035c7cd67dd305e183d93279dafa50
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-06-13 21:59:13 +0200

    Fail test for editing an Idea

--- test/system/ideas_test.rb
+++ test/system/ideas_test.rb
@@ -1,4 +1,6 @@
-require "application_system_test_case"
+# frozen_string_literal: true
+
+require 'application_system_test_case'
 
 class IdeasTest < ApplicationSystemTestCase
   test 'create new idea' do
@@ -22,5 +24,18 @@ class IdeasTest < ApplicationSystemTestCase
     visit(ideas_index_path)
     assert page.has_content?('Cycle across Australia')
     assert page.has_content?('See a giraffe in the wild')
+  end
+
+  test 'Test for editing ideas' do
+    new_idea = Idea.new
+    new_idea.save!
+    visit(edit_idea_path(new_idea))
+    fill_in('done_count', with: 10)
+    fill_in('title', with: 'Build an amazing web app')
+    click_on('Update idea')
+    click_on('Build an amazing web app')
+
+    assert page.has_content?('Build an amazing web app')
+    assert page.has_content?('10 have done this')
   end
 end
