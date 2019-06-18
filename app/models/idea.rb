94145574368df5c401b9cd02221e224cdd11fa52
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-06-18 16:35:49 +0200

    Ensure maximum length of title

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Idea < ApplicationRecord
+  validates :title, presence: true
+  validates :title, length: { maximum: 75 }
+
   def self.most_recent
     all.order(created_at: :desc).limit(3)
   end
