5b7ada9c79d999e1f4d130ae0cfb2cc182c8e4c5
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-06-19 12:00:20 +0200

    Declare nested resouce for Comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root to: 'home#index'
   get 'home/index'
   get 'styles/atoms'
   get 'styles/molecules'
   get 'styles/organisms'
-  resources :ideas
+  resources :ideas do
+    resources :comments
+  end
   get '/account/ideas'
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
