af50109291f1e6789e3c92735b0990597e7738be
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-06-24 10:48:31 +0200

    Associate logged in User new Comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,11 @@
 
 class CommentsController < ApplicationController
   def create
+    user = User.find(session[:user_id])
     comment = Comment.new comment_params
     idea = Idea.find(params[:idea_id])
     comment.idea = idea
+    comment.user = user
     comment.save
 
     redirect_to idea_path(idea)
