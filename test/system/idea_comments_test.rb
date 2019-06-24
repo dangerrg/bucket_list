374d573b4c3b798c42e3a6310dafa621227400f4
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-06-24 10:32:44 +0200

    Log in before creating a Comment in test

--- test/system/idea_comments_test.rb
+++ test/system/idea_comments_test.rb
@@ -4,8 +4,16 @@ require 'application_system_test_case'
 
 class IdeaCommentsTest < ApplicationSystemTestCase
   test 'adding a Comment to an Idea' do
+    user = User.new email: 'user_email@epfl.ch'
+    user.save!
+
     idea = Idea.new title: 'Commented idea'
     idea.save
+
+    visit(new_user_path)
+    fill_in('Email', with: user.email)
+    click_on('Log in')
+
     visit(idea_path(idea))
     fill_in('Add a comment', with: 'This idea has been commented')
     click_on('Post', match: :first)
