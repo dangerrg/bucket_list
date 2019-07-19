6af28be5564ffe825968640c6922a1ee7b60fb57
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-07-19 10:52:32 +0200

    Add password field to sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email)
+    params.require(:user).permit(:email, :password)
   end
 end
