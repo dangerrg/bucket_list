bdf33031bd2502c39a138e523d9302b83dc3ee39
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-07-19 11:53:55 +0200

    Add SessionsController and resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,25 @@
 
 Rails.application.routes.draw do
   root to: 'home#index'
+
   get 'home/index'
+
   get 'styles/atoms'
+
   get 'styles/molecules'
+
   get 'styles/organisms'
+
+  resources :sessions, only [:new, :create]
+
   resources :users do
     resources :goals
   end
+
   resources :ideas do
     resources :comments
   end
+
   get '/account/ideas'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
