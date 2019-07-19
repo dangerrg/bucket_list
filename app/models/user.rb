9d718c445c586d8f937220599ccebfbeed1f5840
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-07-19 10:44:18 +0200

    Add password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
-# frozen_string_literal: true
-
 class User < ApplicationRecord
   has_many :comments
   has_many :ideas
 
   has_and_belongs_to_many :goals, class_name: 'Idea'
+
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_25_110923) do
+ActiveRecord::Schema.define(version: 2019_07_19_084204) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2019_06_25_110923) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
