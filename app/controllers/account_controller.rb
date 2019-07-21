573eeede0c94fa0ef88f8d3eb1efe44267e4d197
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-07-21 23:54:56 +0200

    Pull up current_user method to ApplicationController

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,8 +1,6 @@
 class AccountController < ApplicationController
   before_action :ensure_authenticated
 
-  helper_method :current_user
-
   def edit
   end
 
@@ -22,10 +20,6 @@ class AccountController < ApplicationController
 
   def ensure_authenticated
     redirect_to login_path unless(logged_in?)
-  end
-
-  def current_user
-    User.find(session[:user_id])
   end
 
   private
