220eefc3f22f18496c30d1c663a52a6763778cfe
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-07-21 19:38:17 +0200

    Add current_user helper method to AccountController

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,5 +1,7 @@
 class AccountController < ApplicationController
   before_action :ensure_authenticated
+  
+  helper_method :current_user
 
   def ideas
     user = User.find(session[:user_id])
@@ -8,5 +10,9 @@ class AccountController < ApplicationController
 
   def ensure_authenticated
     redirect_to login_path unless(logged_in?)
+  end
+
+  def current_user
+    User.find(session[:user_id])
   end
 end
