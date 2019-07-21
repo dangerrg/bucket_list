e6f0909807c64eaeedc626f2e78ca2bac5877de4
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-07-21 16:57:37 +0200

    Only display ideas beloning to the user

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -2,12 +2,11 @@ class AccountController < ApplicationController
   before_action :ensure_authenticated
 
   def ideas
-    @ideas = Idea.all
+    user = User.find(session[:user_id])
+    @ideas = user.ideas
   end
 
   def ensure_authenticated
-    unless(logged_in?)
-      redirect_to login_path
-    end
+    redirect_to login_path unless(logged_in?)
   end
 end
