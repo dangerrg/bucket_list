cd5ec993bc5456b6977dbea17656fd2c698ee654
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-07-22 23:54:42 +0200

    Rename :ensure_owner to :authorize_to_edit_idea

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,9 +1,9 @@
 class IdeasController < ApplicationController
   include RolesHelper
-  
-  before_action :ensure_authenticated, only: [:new, :create, :edit, :update] #this will stop any resources being loaded unnecessarily for anonymous users
-  before_action :load_idea,            only: [:edit, :update] #this will load Idea record before executing `ensure_owner` filter
-  before_action :ensure_owner,         only: [:edit, :update] #this make use of the Idea instance
+
+  before_action :ensure_authenticated,   only: [:new, :create, :edit, :update] #this will stop any resources being loaded unnecessarily for anonymous users
+  before_action :load_idea,              only: [:edit, :update] #this will load Idea record before executing `ensure_owner` filter
+  before_action :authorize_to_edit_idea, only: [:edit, :update] #this make use of the Idea instance
 
   def index
     @search_term = params[:q]
@@ -60,7 +60,7 @@ class IdeasController < ApplicationController
     @idea = Idea.find(params[:id])
   end
 
-  def ensure_owner
+  def authorize_to_edit_idea
     redirect_to(account_path) unless(can_edit?(@idea))
   end
 end
