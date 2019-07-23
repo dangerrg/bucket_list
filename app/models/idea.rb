f34bfb17ee7a481040343c5a69d2deeea1fd1a67
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-07-23 23:28:14 +0200

    Add description_contains scope

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -8,8 +8,9 @@ class Idea < ApplicationRecord
 
   has_and_belongs_to_many :users
 
-  scope :most_recent, -> { order(created_at: :desc).limit(3) }
-  scope :title_contains, ->(term) { where('title LIKE ?', "%#{term}%") }
+  scope :most_recent,          -> { order(created_at: :desc).limit(3) }
+  scope :title_contains,       ->(term) { where('title LIKE ?', "%#{term}%") }
+  scope :description_contains, ->(term) { where('description LIKE ?', "%#{term}%") }
 
   def self.search(search_term)
     wildcard_filter = "%#{search_term}%"
