ce3d8769efc7ba43939a02dd088e2be40bba35ab
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-07-23 15:46:17 +0200

    Ensure new Users have a default role

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,16 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true
 
+  after_initialize :default_role!
   before_validation :downcase_email
 
   private
 
   def downcase_email
     self.email = email.downcase
+  end
+
+  def default_role!
+    self.role ||= 'registered'
   end
 end
