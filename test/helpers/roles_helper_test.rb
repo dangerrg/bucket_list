741325f5173eecde30a47074da5db4f436090251
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-07-23 15:58:51 +0200

    Add first test for RolesHelper

--- test/helpers/roles_helper_test.rb
+++ test/helpers/roles_helper_test.rb
@@ -2,4 +2,11 @@ require 'test_helper'
 
 class RolesHelperTest < ActionView::TestCase
   attr_accessor :current_user
+
+  test 'idea can be edited by owner' do
+    owner = User.create(email: 'owner@epfl.ch', password: 'password')
+    self.current_user = owner
+    idea = Idea.create(title: 'A test idea!', user: owner)
+    assert can_edit?(idea)
+  end
 end
