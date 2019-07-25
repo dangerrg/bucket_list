3c47f3e6a7c7f35e4e428b0d837a66f7d989fa4d
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-07-25 22:52:12 +0200

    Seed three goals for the admin User

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # create an admin User and three registered Users.
-admin = User.create! email: 'danger@epfl.ch', password: 'password', name: 'Danger RG', role: 'admin'
+admin = User.create! email: 'danger@epfl.ch', password: 'password', name: 'Danger', role: 'admin'
 user1 = User.create! email: 'lydia@epfl.ch', password: 'password', name: 'Lydia'
 user2 = User.create! email: 'olivia@epfl.ch', password: 'password', name: 'Olivia'
 user3 = User.create! email: 'herbert@epfl.ch', password: 'password', name: 'Herbert'
@@ -11,3 +11,7 @@ user3 = User.create! email: 'herbert@epfl.ch', password: 'password', name: 'Herb
   Idea.create!  title: "Idea for #{user2.name} #{i}", user: user2
   Idea.create!  title: "Idea for #{user3.name} #{i}", user: user3
 end
+
+admin.goals << user1.ideas.first
+admin.goals << user2.ideas.first
+admin.goals << user3.ideas.first
