b32dea7fe7b3ced87ed233512e1d59c56161e4d8
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-07-25 22:35:49 +0200

    Seed five ideas per user

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,12 @@
 admin = User.create! email: 'danger@epfl.ch', password: 'password', name: 'Danger RG', role: 'admin'
 user1 = User.create! email: 'lydia@epfl.ch', password: 'password', name: 'Lydia'
 user2 = User.create! email: 'olivia@epfl.ch', password: 'password', name: 'Olivia'
-user2 = User.create! email: 'herbert@epfl.ch', password: 'password', name: 'Herbert'
+user3 = User.create! email: 'herbert@epfl.ch', password: 'password', name: 'Herbert'
+
+# create 5 Ideas per User.
+1.upto(5) do |i|
+  Idea.create!  title: "Idea for #{admin.name} #{i}", user: admin
+  Idea.create!  title: "Idea for #{user1.name} #{i}", user: user1
+  Idea.create!  title: "Idea for #{user2.name} #{i}", user: user2
+  Idea.create!  title: "Idea for #{user3.name} #{i}", user: user3
+end
