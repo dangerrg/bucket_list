aca9d2a4efd7823cb9768bce694f9a2d259c4bf0
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-07-25 23:28:33 +0200

    Set results per page for Ideas to 9

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -11,8 +11,7 @@ class Idea < ApplicationRecord
   scope :most_recent,          -> { order(created_at: :desc).limit(3) }
   scope :title_contains,       ->(term) { where('title LIKE ?', "%#{term}%") }
   scope :description_contains, ->(term) { where('description LIKE ?', "%#{term}%") }
-
-#
   scope :search, ->(search_term) { title_contains(search_term).or(description_contains(search_term)) }
 
+  paginates_per 9
 end
