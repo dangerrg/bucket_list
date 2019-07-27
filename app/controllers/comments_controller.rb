930ac059a7b4f66cdd9440f3cdfca5f097f1db84
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-07-27 14:13:00 +0200

    Assign instance variable in create comment and render default view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,11 @@ class CommentsController < ApplicationController
   before_action :ensure_authenticated
 
   def create
-    comment = Comment.new comment_params
+    @comment = Comment.new comment_params
     idea = Idea.find(params[:idea_id])
-    comment.idea = idea
-    comment.user = current_user
-    comment.save
-
-    redirect_to idea_path(idea)
+    @comment.idea = idea
+    @comment.user = current_user
+    @comment.save!
   end
 
   private
