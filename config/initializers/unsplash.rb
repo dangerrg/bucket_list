5cc2d3b93a59ab9b6759a0c8bfa91cc3ccf44b64
Danger <d.rodriguez_galvez@hotmail.com>
2019-08-02 22:48:31 +0200

    Include the unsplash gem

---
+++ config/initializers/unsplash.rb
@@ -0,0 +1,4 @@
+Unsplash.configure do |config|
+  config.application_access_key  = "5fcc7bfbb13e738e0757832de9a5344e8c170bc9455f7ea8209624002d6c65f8"
+  config.utm_source              = "exts-fullstack"
+end
