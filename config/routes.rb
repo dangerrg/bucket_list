1db4f9b465822d97ec875c04254211fb02df30b8
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-08-09 22:43:52 +0200

    Define router scope for selecting locale

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,37 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
-  get 'home/index'
+  scope '/:locale' do
+    root to: 'home#index'
 
-  get 'styles/atoms'
+    get 'home/index'
 
-  get 'styles/molecules'
+    get 'styles/atoms'
 
-  get 'styles/organisms'
+    get 'styles/molecules'
 
-  resources :sessions, only: [:new, :create, :destroy]
+    get 'styles/organisms'
 
-  resources :users, only: [:new, :create, :edit, :update] do
-    resources :goals
+    resources :sessions, only: [:new, :create, :destroy]
+
+    resources :users, only: [:new, :create, :edit, :update] do
+      resources :goals
+    end
+
+    resources :ideas do
+      resources :comments
+      resources :photos
+    end
+
+    get 'account', to: 'account#edit'
+    patch 'account', to: 'account#update'
+    get 'account/ideas'
+    get 'account/goals'
+
+    get 'login', to: 'sessions#new'
+
+    get 'signup', to: 'users#new'
+
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
-
-  resources :ideas do
-    resources :comments
-    resources :photos
-  end
-
-  get 'account', to: 'account#edit'
-  patch 'account', to: 'account#update'
-  get 'account/ideas'
-  get 'account/goals'
-
-  get 'login', to: 'sessions#new'
-
-  get 'signup', to: 'users#new'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
